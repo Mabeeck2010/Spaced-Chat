d4bfcc777ea905fbd68d6fc10ba92665b49827dc
Mabeeck <84404489+Mabeeck@users.noreply.github.com>
2026-05-17 07:36:44 +0200

    Remap sources and update build system

--- src/main/java/mabeeck/spacedchat/mixin/ChatScreenMixin.java
+++ src/main/java/mabeeck/spacedchat/mixin/ChatScreenMixin.java
@@ -1,6 +1,6 @@
 package mabeeck.spacedchat.mixin;
 
-import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
-    @Inject(at = @At("RETURN"), method = "normalize",cancellable = true)
+    @Inject(at = @At("RETURN"), method = "normalizeChatMessage",cancellable = true)
     private void inject(String chatText, CallbackInfoReturnable<String> cir) {
         cir.setReturnValue(chatText);
     }
